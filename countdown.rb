276c53a1dfe13fa258fe67f3d4b8251bff1fa028
Jason Keeling <s2jason@yahoo.com>
2020-03-15 00:22:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
-    countdown_with_sleep(#{number})
+    countdown_with_sleep(number)
     number -= 1
   end
     "HAPPY NEW YEAR!"
