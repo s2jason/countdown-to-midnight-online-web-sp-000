5bab864693def5eb3c1659a5bd1db0e229efba2b
Jason Keeling <s2jason@yahoo.com>
2020-03-15 00:16:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,8 @@
 #write your code here
 
 def countdown(number)
-  #{number} = counter
-  number.times do 
   while number > 0 
-    puts "#{number} SECOND(S)!"
+    puts "number SECOND(S)!"
     counter -= 1
   end
     "HAPPY NEW YEAR!"
